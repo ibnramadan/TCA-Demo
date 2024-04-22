ea29bf4c4ddd2af36442b19d6e767c0f007d5163
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2024-04-22 10:20:02 +0200

    Initial Commit

---
+++ TCA-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA-Demo
+//
+//  Created by mohamed ramadan on 22/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA-Demo/TCA_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_DemoApp.swift
+//  TCA-Demo
+//
+//  Created by mohamed ramadan on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCA_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
